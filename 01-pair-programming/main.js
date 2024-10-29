6d29275f15e3de5b2b5b1b36430939bc7a7b19e8
betalantz <betalantz@gmail.com>
2024-10-29 09:39:55 -0700

    scaffolds 01

---
+++ 01-pair-programming/main.js
@@ -0,0 +1,12 @@
+import './style.css'
+
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    
+    <h1>Hello World!</h1>
+   
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # se-ntl-102824-class-sessions
-
 A repo for all supplemental lessons of the se-ntl-102824 cohort
