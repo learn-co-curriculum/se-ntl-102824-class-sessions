e705b76bc01863eef422f5c00d1017f059fbaea3
betalantz <betalantz@gmail.com>
2024-10-30 08:38:49 -0700

    completes solutions 01
    Co-authored-by: jtrapp18 <jtrapp18@users.noreply.github.com>
    Co-authored-by: Todd Cooper <toddcopper@users.noreply.github.com>

--- 01-pair-programming/main.js
+++ 01-pair-programming/main.js
@@ -1,3 +1,4 @@
+
 import './style.css'
 
 
@@ -9,4 +10,27 @@ document.querySelector('#app').innerHTML = `
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
+// Write a function that takes in a first name and a last name and prints out a greeting to the console. Example: greetPerson("Ada", "Lovelace") would print "Hello, Ada Lovelace!"
+
+function greetPerson(firstName, lastName) {
+  console.log(`Hello, ${firstName} ${lastName}!`)
+}
+
+greetPerson("Ada", "Lovelace")
+
+// Write a function that prints the number of letters in a string. Example: numberOfLetters("Ada") => prints 3 to the console.
+
+function numberOfLetters(word) {
+  // console.log(word.split("").length)
+  console.log(word.length)
+}
+
+numberOfLetters("Ada")
+
+// Write a function that takes in an integer and returns the number of digits in that integer. Example: numberOfDigits(2598) would return 4.
+
+function numberOfDigits(n) {
+  return n.toString().length
+}
+
+numberOfDigits(2598)
