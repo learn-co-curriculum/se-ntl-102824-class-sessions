dc4a132936519487f5d45acb2d82c7ee43a88cc5
betalantz <betalantz@gmail.com>
2024-11-08 11:21:50 -0800

    adds ramen rater to repo 07

---
+++ 07-p1-cc-ramen/src/index.js
@@ -0,0 +1,29 @@
+// index.js
+
+// Callbacks
+const handleClick = (ramen) => {
+  // Add code
+};
+
+const addSubmitListener = () => {
+  // Add code
+}
+
+const displayRamens = () => {
+  // Add code
+};
+
+const main = () => {
+  // Invoke displayRamens here
+  // Invoke addSubmitListener here
+}
+
+main()
+
+// Export functions for testing
+export {
+  displayRamens,
+  addSubmitListener,
+  handleClick,
+  main,
+};
