69bd09fc7f872bfb79203e007c8cd52e5590ded9
betalantz <betalantz@gmail.com>
2024-11-25 14:26:52 -0800

    completes deliv 2 of 08

--- 08-react-hogwild/src/components/HogDetail.jsx
+++ 08-react-hogwild/src/components/HogDetail.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
-function HogDetail() {
+function HogDetail({greased, specialty, weight, "highest medal achieved": medal}) {
   return (
-    <div>HogDetail</div>
+    <div className="description">
+      <strong>{greased ? "Very Greasy" : "Squeaky Clean!"}</strong>
+      <p>
+        Highest medal achieved: <strong>{medal}</strong>
+      </p>
+      <p>
+        Specialty: <strong>{specialty}</strong>
+      </p>
+      <p>
+        Weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator
+        with Thru-the-Door Ice and Water: <strong>{weight}</strong>
+      </p>
+    </div>
   )
 }
 
