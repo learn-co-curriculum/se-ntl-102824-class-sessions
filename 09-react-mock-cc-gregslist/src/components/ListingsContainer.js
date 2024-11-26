0c0262aec87efcbc5b0d5da0952f38bd4590b970
betalantz <betalantz@gmail.com>
2024-11-26 10:44:29 -0800

    1st deliv 09

--- 09-react-mock-cc-gregslist/src/components/ListingsContainer.js
+++ 09-react-mock-cc-gregslist/src/components/ListingsContainer.js
@@ -1,11 +1,14 @@
 import React from "react";
-// import ListingCard from "./ListingCard";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({ listings }) {
+
+  const listingCards = listings.map(listItem => <ListingCard key={listItem.id} listing={listItem} />)
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listingCards}
       </ul>
     </main>
   );
