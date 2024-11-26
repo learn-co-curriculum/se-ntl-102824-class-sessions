baa34d81d56245d061a5b61cb7933a422f7f3e7a
betalantz <betalantz@gmail.com>
2024-11-26 11:48:56 -0800

    complete core deliverables 09

--- 09-react-mock-cc-gregslist/src/components/App.js
+++ 09-react-mock-cc-gregslist/src/components/App.js
@@ -2,34 +2,47 @@ import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
-const baseURL = "http://localhost:6001"
+const baseURL = "http://localhost:6001";
 function App() {
-
-  const [listings, setListings] = useState([])
+  const [listings, setListings] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     fetch(baseURL + "/listings")
-      .then(response => response.json())
-      .then(setListings)
-  }, [])
+      .then((response) => response.json())
+      .then(setListings);
+  }, []);
 
-  function removeListing(listingId){
-    console.log("🚀 ~ removeListing ~ listingId:", listingId)
-    fetch(`${baseURL}/listings/${listingId}`, { method: 'DELETE' })
-      .then(r => {
+  function removeListing(listingId) {
+    console.log("🚀 ~ removeListing ~ listingId:", listingId);
+    fetch(`${baseURL}/listings/${listingId}`, { method: "DELETE" }) // persists to backend
+      .then((r) => {
         if (r.ok) {
-          setListings(listings.filter(listing => listing.id !== listingId)) // pessimistic rendering
+          setListings(listings.filter((listing) => listing.id !== listingId)); // pessimistic rendering
         } else {
-          throw new Error("Couldn't remove listing")
+          throw new Error("Couldn't remove listing");
         }
-      })
+      });
     // setListings(listings.filter(listing => listing.id !== listingId)) // optimistic rendering
+  }
+
+  function changeSearch(newSearchTerm) {
+    setSearchTerm(newSearchTerm);
+  }
+
+  function filterListings() {
+    return listings.filter((listing) =>
+      listing.description.toLowerCase().includes(searchTerm.toLowerCase())
+    );
   }
 
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer listings={listings} onDelete={removeListing}/>
+      <Header onSearch={changeSearch} />
+      <ListingsContainer
+        listings={filterListings()}
+        onDelete={removeListing}
+      />
     </div>
   );
 }
