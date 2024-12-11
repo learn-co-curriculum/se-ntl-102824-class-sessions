8a83c0122132c6cb2639259fac99d8bbbedfa0cd
betalantz <betalantz@gmail.com>
2024-12-11 09:04:21 -0800

    solution 10

--- 10-python-badges-schedules/lib/conference_badges.py
+++ 10-python-badges-schedules/lib/conference_badges.py
@@ -1,11 +1,33 @@
+import ipdb
+
+
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
+
 
 def batch_badge_creator(names):
-    return None
+    return [badge_maker(name) for name in names]
+
 
 def assign_rooms(names):
-    return None
+    # assignments = []
+    # for i, name in enumerate(names, start=1):
+    #     ipdb.set_trace()
+    #     assignments.append(f"Hello, {name}! You'll be assigned to room {i}!")
+    # return assignments
+    return [
+        f"Hello, {name}! You'll be assigned to room {i}!"
+        for i, name in enumerate(names, start=1)
+    ]
+
+
+assign_rooms(["Arel", "Carol"])
+
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    assignments = assign_rooms(names)
+    for badge in badges:
+        print(badge)
+    for assignment in assignments:
+        print(assignment)
