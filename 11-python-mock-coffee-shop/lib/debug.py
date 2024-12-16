96e88a321adcf8f5b08ed56d74f6e10b1e369947
betalantz <betalantz@gmail.com>
2024-12-16 11:36:42 -0800

    adds implementation code to debug.py

--- 11-python-mock-coffee-shop/lib/debug.py
+++ 11-python-mock-coffee-shop/lib/debug.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 import ipdb
+from classes.many_to_many import Coffee, Customer, Order
 
-from classes.many_to_many import Customer
-from classes.many_to_many import Order
-from classes.many_to_many import Coffee
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("HELLO! :) let's debug")
+
+    coffee_1 = Coffee("Mocha")
+    coffee_2 = Coffee("Vanilla Latte")
+
+    customer = Customer("Steve")
+    customer_2 = Customer("Dima")
+
+    customer.create_order(coffee_1, 5.95)
+    customer.create_order(coffee_2, 4.99)
+    customer_2.create_order(coffee_1, 4.85)
+    customer_2.create_order(coffee_2, 5.55)
 
     ipdb.set_trace()
