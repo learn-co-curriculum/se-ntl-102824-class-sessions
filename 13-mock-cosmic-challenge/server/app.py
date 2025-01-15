debbb017363891a458e19fed3c7684e570dcbf72
betalantz <betalantz@gmail.com>
2025-01-15 08:01:30 -0800

    completes routes through destroy

--- 13-mock-cosmic-challenge/server/app.py
+++ 13-mock-cosmic-challenge/server/app.py
@@ -32,7 +32,34 @@ class Scientists(Resource):
         scis = [sci.to_dict(only=("id", "name", "field_of_study")) for sci in Scientist.query.all()]
         return make_response(scis, 200)
     
+class ScientistById(Resource):
+
+    @classmethod
+    def find_sci(cls, id):
+            sci = Scientist.query.get(id)
+            print(sci)
+            return sci
+        
+    def get(self, id):
+        sci = self.__class__.find_sci(id)
+        if not sci:
+             return make_response({"error": "Scientist not found"}, 404)
+        return make_response(sci.to_dict(), 200)
+    
+    def delete(self, id):
+        sci = self.__class__.find_sci(id)
+        if not sci:
+             return make_response({"error": "Scientist not found"}, 404)
+        db.session.delete(sci)
+        db.session.commit()
+        return make_response("", 204)
+    
 api.add_resource(Scientists, "/scientists")
+api.add_resource(ScientistById, "/scientists/<int:id>")
+
+# @app.register_error_handler(404)
+# def handle_404(e):
+#      return make_response({ "error": f"{request.path}.replace("/1234567890").title()"})
 
 
 if __name__ == '__main__':
